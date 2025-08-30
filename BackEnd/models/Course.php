d981b27b02a13fc4e4bf8297d30410fcb54a1413
Eliardo <eliardosilva81@gmail.com>
2025-08-30 14:37:58 -0300

    refactor: mudando nome de parametro

--- BackEnd/models/Course.php
+++ BackEnd/models/Course.php
@@ -4,17 +4,17 @@ class Course {
     public $id;
     public $imageUrl;
     public $imageAlt;
-    public $title1;
-    public $title2;
+    public $titleOne;
+    public $titleTwo;
     public $description;
     public $additionalInfo;
 
-    public function __construct($id, $imageUrl, $imageAlt, $title1, $title2, $description, $additionalInfo) {
+    public function __construct($id, $imageUrl, $imageAlt, $titleOne, $titleTwo, $description, $additionalInfo) {
         $this->id = $id;
         $this->imageUrl = $imageUrl;
         $this->imageAlt = $imageAlt;
-        $this->title1 = $title1;
-        $this->title2 = $title2;
+        $this->titleOne = $titleOne;
+        $this->titleTwo = $titleTwo;
         $this->description = $description;
         $this->additionalInfo = $additionalInfo;
     }
